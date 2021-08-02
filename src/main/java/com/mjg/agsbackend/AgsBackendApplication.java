a6115b08aca254d795cb2efd6d9b19a27028bc06
Michael Green <etovian@gmail.com>
2021-08-02 07:17:44 -0600

    initial commit

---
+++ src/main/java/com/mjg/agsbackend/AgsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mjg.agsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjg/agsbackend/AgsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjg.agsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
